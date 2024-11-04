ccc5472b20bcf49306af45ea11523849d6187981
Hieu <hieuht2910@gmail.com>
2024-11-04 22:25:28 +0700

    update

--- import_es.py
+++ import_es.py
@@ -2,15 +2,15 @@ import csv
 from datetime import datetime
 from elasticsearch import Elasticsearch, helpers
 
-es = Elasticsearch(
-    ['http://localhost:9200'],
-    basic_auth=('elastic', 'changeme')
-)
-
 # es = Elasticsearch(
-#     ['http://192.168.200.161:9200'],
-#     basic_auth=('elastic', 'hust@2024')
+#     ['http://localhost:9200'],
+#     basic_auth=('elastic', 'changeme')
 # )
+
+es = Elasticsearch(
+    ['http://192.168.200.161:9200'],
+    basic_auth=('elastic', 'hust@2024')
+)
 
 # Define the index name and mapping
 index_name = 'amazon_stock_2'
@@ -32,40 +32,40 @@ mapping = {
 es.indices.create(index=index_name, body=mapping, ignore=400)
 
 
-def read_csv_and_index(file_path):
-    with open(file_path, 'r') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',')
-
-        actions = []
-        for row in reader:
-            # Convert date string to datetime object
-            # date = datetime.strptime(row['Date'], '%Y-%m-%d')
-            # formatted_date = date.strftime('%Y-%m-%d %H:%M:%S.%f')
-            # print(formatted_date)
-            # Create document
-            doc = {
-                '_index': index_name,
-                '_id': row['Date'],
-                '_source': {
-                    'Date': row['Date'],
-                    'Open': float(row['Open']),
-                    'High': float(row['High']),
-                    'Low': float(row['Low']),
-                    'Close': float(row['Close']),
-                    'Adj Close': float(row['Adj Close']),
-                    'Volume': int(row['Volume'])
-                }
-            }
-            actions.append(doc)
-
-            # If we have 100 documents, send them to Elasticsearch
-            if len(actions) == 100:
-                helpers.bulk(es, actions)
-                actions = []
-
-        # Send any remaining documents
-        if actions:
-            helpers.bulk(es, actions)
+# def read_csv_and_index(file_path):
+#     with open(file_path, 'r') as csvfile:
+#         reader = csv.DictReader(csvfile, delimiter=',')
+#
+#         actions = []
+#         for row in reader:
+#             # Convert date string to datetime object
+#             # date = datetime.strptime(row['Date'], '%Y-%m-%d')
+#             # formatted_date = date.strftime('%Y-%m-%d %H:%M:%S.%f')
+#             # print(formatted_date)
+#             # Create document
+#             doc = {
+#                 '_index': index_name,
+#                 '_id': row['Date'],
+#                 '_source': {
+#                     'Date': row['Date'],
+#                     'Open': float(row['Open']),
+#                     'High': float(row['High']),
+#                     'Low': float(row['Low']),
+#                     'Close': float(row['Close']),
+#                     'Adj Close': float(row['Adj Close']),
+#                     'Volume': int(row['Volume'])
+#                 }
+#             }
+#             actions.append(doc)
+#
+#             # If we have 100 documents, send them to Elasticsearch
+#             if len(actions) == 100:
+#                 helpers.bulk(es, actions)
+#                 actions = []
+#
+#         # Send any remaining documents
+#         if actions:
+#             helpers.bulk(es, actions)
 
 def publish_to_elasticsearch(row_data):
     actions = []
@@ -105,6 +105,5 @@ def publish_to_elasticsearch(row_data):
     print("Data indexing complete.")
 
 # Run the function
-read_csv_and_index('AMZN_kaggle.csv')
-
-print("Data indexing complete.")
+# read_csv_and_index('AMZN_kaggle.csv')
+# print("Data indexing complete.")
